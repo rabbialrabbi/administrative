5af94f20fcb2f591f13da7bf3d77244d51ef631e
Rabbial Anower Home <rabbialrabbi@gmail.com>
2020-05-12 05:45:46 +0600

    Deploy to Heroku

---
+++ resources/views/layouts/adminBodyLayout.blade.php
@@ -0,0 +1,40 @@
+
+{{--***************************
+            @ Table data
+     ***************************** --}}
+<div class="row body_top">
+    <div class="col-2"><h3>প্রশাসনিক</h3> </div>
+    <div class="col-1 clone">:</div>
+    <div class="col-8"><h3>{{$section}}</h3></div>
+</div>
+<div class="row body_search-panel">
+    <div class="col-8"></div>
+    <div class="col-4">
+        <input type="text">
+        <input type="text">
+        <span class="dropdownIcon">...</span>
+    </div>
+</div>
+
+{{--****************************************************************
+        @ data from database will fetch with ajex and append here
+ ******************************************************************* --}}
+<div id="indexData" class="row body_bottom">
+    <img src="https://cdn.clipart.email/d94ddd93994b3bc30cc1dd53c5e2a09e_spinning-circle-png-gif_480-474.gif" alt="Flowers in Chania" height="80" width="80" style="margin-left: auto;margin-right: auto">
+</div>
+
+{{--****************************************************************
+        @ Below div containt pageination and button
+ ******************************************************************* --}}
+<div class="row body_pagination">
+    <div class="col-8 paginator js-paginator d-flex justify-content-center"></div>
+    <div class="col-4">
+        <button id="addDivision">অ্যাড</button>
+    </div>
+</div>
+
+{{--**********************************************************
+    @ Sub Table Body
+    @ data from database will fetch with ajex and append here
+*************************************************************** --}}
+<div id="sub_input"></div>
--- resources/views/pages/upazilaPage.blade.php
+++ resources/views/pages/upazilaPage.blade.php
@@ -1,48 +1,9 @@
 @extends('layouts.adminLayout')
 
-@section('title','Admin')
+@section('title','উপজেলা')
 @section('body')
 
-    {{--***************************
-            @ Table data
-     ***************************** --}}
-    <div class="row body_top">
-        <div class="col-2"><h3>প্রশাসনিক</h3> </div>
-        <div class="col-1 clone">:</div>
-        <div class="col-8"><h3>উপজেলা</h3></div>
-    </div>
-    <div class="row body_search-panel">
-        <div class="col-8"></div>
-        <div class="col-4">
-            <input type="text">
-            <input type="text">
-            <span class="dropdownIcon">...</span>
-        </div>
-    </div>
-
-    {{--****************************************************************
-            @ data from database will fetch with ajex and append here
-     ******************************************************************* --}}
-    <div id="indexData" class="row body_bottom">
-        <img src="https://cdn.clipart.email/d94ddd93994b3bc30cc1dd53c5e2a09e_spinning-circle-png-gif_480-474.gif" alt="Flowers in Chania" height="80" width="80" style="margin-left: auto;margin-right: auto">
-    </div>
-
-    {{--****************************************************************
-            @ Below div containt pageination and button
-     ******************************************************************* --}}
-    <div class="row body_pagination">
-        <div class="col-8 paginator js-paginator d-flex justify-content-center"></div>
-        <div class="col-4">
-            <button id="addDivision">অ্যাড</button>
-        </div>
-    </div>
-
-    {{--**********************************************************
-        @ Sub Table Body
-        @ data from database will fetch with ajex and append here
-    *************************************************************** --}}
-    <div id="sub_input"></div>
-
+    @include('layouts.adminBodyLayout',['section'=>'উপজেলা'])
 
 @endsection
 
